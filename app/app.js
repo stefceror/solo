63ca0464c6163935f0f9494c69be177fd33da065
Stefanie Contreras <stefsnow@gmail.com>
2014-08-25 14:21:15 -0700

    add controller for button on pattern page

--- app/app.js
+++ app/app.js
@@ -1,11 +1,18 @@
 var chartify = angular.module('chartify', [
   'ngRoute'
-]);
+])
+  .controller('PatternController',['$scope',
+  function($scope){
+    $scope.chart= function(){
+      console.log('chart clicked');
+    };
+}]);
 
 chartify.config(['$routeProvider', function($routeProvider){
   $routeProvider
     .when('/', {
-      templateUrl: 'pattern-text.html'
+      templateUrl: 'pattern-text.html',
+      controller: 'PatternController'
     })
     .when('/chart', {
       templateUrl: 'chart.html'
@@ -14,3 +21,5 @@ chartify.config(['$routeProvider', function($routeProvider){
       redirectTo: '/'
     });
 }]);
+
+
